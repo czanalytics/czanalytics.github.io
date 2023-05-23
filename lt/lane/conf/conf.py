d168146050e5cd12ad713c4f03027112b4164e96
czanalytics <aatos.heikkinen@czanalytics.com>
2023-05-23 16:37:29 +0300

    adding docstring

--- lt/lane/conf/conf.py
+++ lt/lane/conf/conf.py
@@ -1,10 +1,13 @@
-print('Load Tripping')
 
-def conf():
+def hello():
+    print('Hello from Lane :)
+
+def conf2():
+          """Default Lane configation"""
     lt = {'a':1, 'ab':'xxx'}
     return lt
 
-def foo(x):
+def foo2(x):
     return (x,x*2)
 
 
