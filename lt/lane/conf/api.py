3eaebf6264d5f48d8cf58a23f0d022641c5ea4a6
czanalytics <aatos.heikkinen@czanalytics.com>
2023-05-29 10:19:30 +0300

    json conf added

--- lt/lane/conf/api.py
+++ lt/lane/conf/api.py
@@ -2,5 +2,16 @@
 def hello():
     print('Hello from Lane api')
 
-s=hello()
+s = hello()
 
+api_obj = {
+    'boolean': False,
+    'dict': {'a': 1, 'b': 2, 'c': 3},
+    'int': 1,
+    'float': 3.1,
+    'list': [1, 2, 3],
+    'null': None,
+    'string': 'A string',
+}
+
+#
