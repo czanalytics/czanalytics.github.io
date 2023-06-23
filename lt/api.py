b28861dde452800b53232bc227ee293d8e902c4f
czanalytics <aatos.heikkinen@czanalytics.com>
2023-06-23 13:49:16 +0300

    introducing error estimates to price/eta/co endpoints

--- lt/api.py
+++ lt/api.py
@@ -3,7 +3,6 @@ from flask_restful import Resource, Api
 from flask_expects_json import expects_json
 
 from datetime import datetime, date
-#import datetime
 import json
 
 # https://apiflask.com/api/app/
@@ -28,26 +27,51 @@ price_schema =  {'type': 'object',
                  'required': ['mode']
                  }
 
-@app.route('/api/bar', methods=['GET', 'POST'])
-@expects_json(price_schema)
-def bar():
-    return 'bar'
+
+#@app.route('/api/bar', methods=['GET', 'POST'])
+#@expects_json(price_schema)
+#def bar():
+#    return 'bar'
 
 #@app.route('/api/foo', methods=['GET', 'POST'])
 
-def price_est(distance):
-    per_km = 1
-    min_fee = 100*per_km
-    return (min_fee + distance * per_km)
+def price_est(d, price_km=2, price_min=100, err=0.1):
+    """
+    Simplistic transportation price estimate
+    price, p [EUR] =  straight_line(distance, d [km], price/km [EUR/km], price_min [EUR])
+    """
+    p = price_km * d + price_min
+    p_lo = p - p * err
+    p_hi = p + p * err
 
-def co2_est(distance, mode):
-    return 1
+    return p, p_lo, p_hi
 
-class foo(Resource):
+def eta_est(d, v=100, err=0.1):
+    """
+    Simplistic transportation time estimate
+    time, t [h] = distance, d [km] / speed, v [km/h]
+    """
+    t = d / v
+    t_lo = t - t * err
+    t_hi = t + t * err
+    return t, t_lo, t_hi
+
+def co_est(d, c=100, err=0.2):
+    """
+    Simplistic transportation CO2 exhaust estimate
+    CO2 [g] =  distance, d [km] * CO2, c [g/km]
+    """
+    co = d * c
+    co_lo = co - co * err
+    co_hi = co + co * err
+    return co, co_lo, co_hi
+
+class price(Resource):
     def get(self):
         req = request.json
         dist = req['distance']
-        est = price_est(dist)
+
+        p, p_lo, p_hi = price_est(dist) # !
 
         d = {'left': 0.17037454,
              'right': 0.82339555,
@@ -55,7 +79,9 @@ class foo(Resource):
              }
 
         message = {
-            'price': est,
+            'price': p,
+            'price_lo': p_lo,
+            'price_hi': p_hi,
             'datetime': datetime.now(),
             'req': req
         }
@@ -74,16 +100,44 @@ class foo(Resource):
         )
         #return 'POST'
 
-class bar(Resource):
+class eta(Resource):
     def get(self):
         req = request.json
-        dist = req['distance']
-        #mode = req['mode']
-        #est = price_est(dist)
-        est = distance*0.2
+        d = req['distance']
+
+        t, t_lo, t_hi = eta_est(d) # !
 
         message = {
-            'co2': est,
+            'eta': t,
+            'eta_lo': t_lo,
+            'eta_hi': t_hi,
+            'datetime': datetime.now(),
+            'req': req
+        }
+
+        resp = jsonify(message)
+        resp.status_code = 200
+
+        print(resp)
+        return resp
+
+    def post(self):
+        return jsonify(
+            greeting=["foo", "post"],
+            date=datetime.today(),
+        )
+
+class co(Resource):
+    def get(self):
+        req = request.json
+        d = req['distance']
+
+        co, co_lo, co_hi = co_est(d) # !
+
+        message = {
+            'co': co,
+            'co_lo': co_lo,
+            'co_hi': co_hi,
             'datetime': datetime.now(),
             'req': req
         }
@@ -119,7 +173,7 @@ def foo2():
 #   else:
 #      return make_response(jsonify({'distance': 1000}), 200)
 
-
+"""
 class price(Resource):
     def get(self):
         #return 'Price'
@@ -135,23 +189,21 @@ class eta(Resource):
     def post(self):
         pass
 
-class carbon(Resource):
+class co(Resource):
     def get(self):
         return jsonify(id=1111, mode="truck", distance=1000, co2=666)
 
     def post(self):
         pass
+"""
 
 api.add_resource(api_greet, '/', endpoint='/') # Route_1
 
 api.add_resource(api_greet, '/api', endpoint='/api') #
 
-api.add_resource(price,  '/api/price',  endpoint='/api/price') #
-api.add_resource(eta,    '/api/eta',    endpoint='/api/eta') #
-api.add_resource(carbon, '/api/carbon', endpoint='/api/carbon') #
-
-api.add_resource(foo, '/api/foo', endpoint='/api/foo') #
-api.add_resource(bar, '/api/bar', endpoint='/api/bar') #
+api.add_resource(price, '/api/price', endpoint='/api/price')
+api.add_resource(eta,   '/api/eta',   endpoint='/api/eta')
+api.add_resource(co,    '/api/co',    endpoint='/api/co')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', '3333')
--- lt/api.sh
+++ lt/api.sh
@@ -1,11 +1,11 @@
 # api.sh
 #
-# source api.sh && api > tmp.txt ; vim tmp.txt
+# source api.sh && api > tmp.txt; vim tmp.txt
 api() { 
  echo "fn:"${FUNCNAME[*]}
  DateTag=$(date)
  echo "   : " $DateTag
- set -x # set echo, set +x unsets
+ set -x # shell echo, set +x unsets
 
  con="api"
  conn="$con"_con
@@ -28,37 +28,39 @@ api() {
 
  hjson="Content-type: application/json"
  
- d1='{"mode":"truck","distance":1000}'
- d1a='{"cargo_id":"00001", "lane":1, "mode":"trailer.o1", "distance":100}'
- d1b='{"cargo_id":"00002", "lane":1, "mode":"trailer.o4", "distance":1000}'
- d1c='{"cargo_id":"00003", "lane":1, "mode":"trailer.o4", "distance":500}'
- d1d='{"cargo_id":"00003", "lane":2, "mode":"roro",       "distance":500}'
+
+ d1='{"id":"00001", "lane":1, "mode":"trailer.o1", "distance":10}'
+ d2='{"id":"00002", "lane":2, "mode":"trailer.o4", "distance":100}'
+ d3='{"id":"00003", "lane":3, "mode":"trailer.o4", "distance":500}'
+ d4='{"id":"00004", "lane":4, "mode":"roro",       "distance":1000}'
 
  echo "curl>>"
 
  curl -s -v $url
 
  curl -s "$url"/api        | "$pp"
- curl -s "$url"/api/price  | "$pp"
- curl -s "$url"/api/eta    | "$pp"
- curl -s "$url"/api/carbon | "$pp"
+
+ # price
+ curl -s -X GET  -H "$hjson" $url/api/price --data "$d1" | "$pp" # prettyprinted with silent -s
+ curl -s -X GET  -H "$hjson" $url/api/price --data "$d2" | "$pp"
+ curl -s -X GET  -H "$hjson" $url/api/price --data "$d3" | "$pp"
+ curl -s -X GET  -H "$hjson" $url/api/price --data "$d4" | "$pp"
+
+ # eta
+ curl -s -X GET  -H "$hjson" $url/api/eta   --data "$d1" | "$pp"
+ curl -s -X GET  -H "$hjson" $url/api/eta   --data "$d2" | "$pp"
+ curl -s -X GET  -H "$hjson" $url/api/eta   --data "$d3" | "$pp"
+ curl -s -X GET  -H "$hjson" $url/api/eta   --data "$d4" | "$pp"
  
- # -s for silent
- #curl -s -X GET  -H "$hjson" $url/api/foo --data "$d1" | "$pp"
+ # co
+ curl -s -X GET  -H "$hjson" $url/api/co    --data "$d1" | "$pp"
+ curl -s -X GET  -H "$hjson" $url/api/co    --data "$d2" | "$pp"
+ curl -s -X GET  -H "$hjson" $url/api/co    --data "$d3" | "$pp"
+ curl -s -X GET  -H "$hjson" $url/api/co    --data "$d4" | "$pp"
 
- # foo
- curl -s -X GET  -H "$hjson" $url/api/foo --data "$d1a" | "$pp" # prettyprinted with silent -s
- curl -s -X GET  -H "$hjson" $url/api/foo --data "$d1b" | "$pp"
- curl -s -X GET  -H "$hjson" $url/api/foo --data "$d1c" | "$pp"
- curl -s -X GET  -H "$hjson" $url/api/foo --data "$d1d" | "$pp"
-
- # bar
- curl -s -X GET  -H "$hjson" $url/api/bar --data "$d1c" | "$pp"
- curl -s -X GET  -H "$hjson" $url/api/bar --data "$d1d" | "$pp"
-
- #curl -s -X POST -H "$hjson" $url/api/foo --data "$d1" | "$pp"
  
  echo "<<curl"
+
  set +x
  docker logs -t $conn
  echo $(date)
