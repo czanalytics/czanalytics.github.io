e2b865dee2fb083b1169a12992ef7561c14dfc8f
czanalytics <aatos.heikkinen@czanalytics.com>
2023-06-29 09:33:53 +0300

    clean and clarify

--- lt/lane.py
+++ lt/lane.py
@@ -4,13 +4,11 @@
 # https://apiflask.com/api/app/
 # https://github.com/Fischerfredl/flask-expects-json
 # https://lovelace.oulu.fi/ohjelmoitava-web/programmable-web-project-spring-2019/implementing-rest-apis-with-flask/
-# pip install jsonschema ?
 # curl https://stackoverflow.com/questions/52133268/flask-get-requests-with-json-parameter-using-curl
 # https://stackoverflow.com/questions/13081532/return-json-response-from-flask-view
-
 # https://www.toptal.com/python/in-depth-python-logging
-import logging as log # log.debug/info/warning 
 
+import logging as log # log.debug/info/warning 
 import json
 
 schema_api = {'type': 'object',
@@ -19,16 +17,16 @@ schema_api = {'type': 'object',
                          'required': ['date', 'from_lat']
           }
 
-ms = {'price': 'price_simple', 'eta': 'eta_simple','co': 'co_simple' }
-#ms = {'price': 'price_lite', 'eta': 'eta_lite','co': 'co_lite' }
-#ms = {'price': 'price_gam', 'eta': 'eta_gam','co': 'co_gam' }
-#ms = {'price': 'price_automl', 'eta': 'eta_automl','co': 'co_automl' }
+mods = {'price': 'price_simple', 'eta': 'eta_simple','co': 'co_simple' }
+#mods = {'price': 'price_lite', 'eta': 'eta_lite','co': 'co_lite' }
+#mods = {'price': 'price_gam', 'eta': 'eta_gam','co': 'co_gam' }
+#mods = {'price': 'price_automl', 'eta': 'eta_automl','co': 'co_automl' }
 
 conf = {'version': 0.27,
         'app_ip': '0.0.0.0',
         'app_port': 3333,
         'schema': schema_api,
-        'model': ms
+        'model': mods
         }
 
 l = log.DEBUG
@@ -37,22 +35,22 @@ log.basicConfig(format='%(asctime)s, %(name)s, %(levelname)s, %(funcName)s:%(lin
 #log.basicConfig(format='%(asctime)s, %(levelname)s, %(message)s', level=log.DEBUG)
 
 
-def price_est(dj, model):
+def price_est(d, mod):
     """
-    Dispatch price calculation to requsted model
+    Dispatch price calculation to requested model
     """
-    match model:
+    match mod:
         case 'price_simple':
-            p, p_lo, p_hi = price_simple(dj)
+            p, p_lo, p_hi = price_simple(d)
         case 'price_lite':
             p, p_lo, p_hi = 0, 0, 0
-            #t, t_lo, t_hi = price_lite(dj)
+            #t, t_lo, t_hi = price_lite(d)
         case 'price_gam':
             p, p_lo, p_hi = 0, 0, 0
-            #t, t_lo, t_hi = price_gam(dj)
+            #t, t_lo, t_hi = price_gam(d)
         case 'price_automl':
             p, p_lo, p_hi = 0, 0, 0
-            #p, p_lo, p_hi = price_automl(dj)
+            #p, p_lo, p_hi = price_automl(d)
         case _:
             p, p_lo, p_hi = 0, 0, 0
             #log.ERROR('unknown model') # fails
@@ -60,39 +58,39 @@ def price_est(dj, model):
     return round(p), round(p_lo), round(p_hi)
 
 
-def eta_est(dj, model):
+def eta_est(d, mod):
     """
     Dispatch ETA calculation to requested model
     """
     # structural pattern mathing, https://peps.python.org/pep-0622/
-    match model:
+    match mod:
         case 'eta_simple':
-            t, t_lo, t_hi = eta_simple(dj)
+            t, t_lo, t_hi = eta_simple(d)
         case 'eta_lite':
             t, t_lo, t_hi = 0, 0, 0
-            #t, t_lo, t_hi = eta_lite(dj)
+            #t, t_lo, t_hi = eta_lite(d)
         case 'eta_gam':
             t, t_lo, t_hi = 0, 0, 0
-            #t, t_lo, t_hi = eta_gam(dj)
+            #t, t_lo, t_hi = eta_gam(d)
         case _:
             t, t_lo, t_hi = 0, 0, 0
 
     return round(t, 1), round(t_lo, 1), round(t_hi, 1)
 
 
-def co_est(dj, model):
+def co_est(d, mod):
     """
     Dispatch CO2 estimation to requested model
     """
-    match model:
+    match mod:
         case 'co_simple':
-            co, co_lo, co_hi = co_simple(dj)
+            co, co_lo, co_hi = co_simple(d)
         case 'co_lite':
             co, co_lo, co_hi = 0, 0, 0
-            #co, co_lo, co_hi = co_lite(dj)
+            #co, co_lo, co_hi = co_lite(d)
         case 'co_gam':
             co, co_lo, co_hi = 0, 0, 0
-            #co, co_lo, co_hi = co_gam(dj)
+            #co, co_lo, co_hi = co_gam(d)
         case _:
             co, co_lo, co_hi = 0, 0, 0
 
@@ -123,37 +121,33 @@ def dist(d):
 
     lat1 = radians(d['from_lat'])
     lon1 = radians(d['from_lon'])
+
     lat2 = radians(d['to_lat'])
     lon2 = radians(d['to_lon'])
 
-    #status = "connection unavailable"
-    #log.error("System reported: %s", status)
+    lond = lon2 - lon1
+    latd = lat2 - lat1
 
-    # Approximate radius of earth in km
-    R = 6373.0
-
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    a = sin(latd/2)**2 + cos(lat1) * cos(lat2) * sin(lond/2)**2
     c = 2 * atan2(sqrt(a), sqrt(1-a))
 
-    dist = round(R * c)
+    r = 6373.0 # earth radius [km]
+    dist = round(r * c)
 
     log.debug('dist = %i', dist)
 
     return dist
 
 
-def price_gam(dj, price_km=2, price_min=50, err=0.1):
+def price_gam(d, price_km=2, price_min=50, err=0.1):
     """
-    ML based transportation price estimate
-    price, p [EUR] =  straight_line(distance, d [km], price/km [EUR/km], price_min [EUR])
+    ML based transportation price estimate.
+    GAM model uses EU statistics, price-distance matrix between NUTS regions.
     """
-    d = dist(dj)
+    l = dist(d)
 
-    from_reg = region(lat=dj["from_lat"], lon=dj["from_lon"])
-    to_reg = region(lat=dj["to_lat"], lon=dj["to_lon"])
+    from_reg = region(lat=d["from_lat"], lon=d["from_lon"])
+    to_reg = region(lat=d["to_lat"], lon=d["to_lon"])
 
     log.debug('from: %s', from_reg)
     log.debug('to  : %s', to_reg)
@@ -164,42 +158,42 @@ def price_gam(dj, price_km=2, price_min=50, err=0.1):
     return round(p), round(p_lo), round(p_hi)
 
 
-def price_simple(dj, price_km=2, price_min=50, err=0.1):
+def price_simple(d, price_km=2, price_min=50, err=0.1):
     """
     Simplistic transportation price estimate
-    price, p [EUR] =  straight_line(distance, d [km], price/km [EUR/km], price_min [EUR])
+    price, p [EUR] =  straight_line(length, l [km], price/km [EUR/km], price_min [EUR])
     """
-    d = dist(dj)
+    l = dist(d)
 
-    p = price_km * d + price_min
+    p = price_km * l + price_min
     p_lo = p - p * err
     p_hi = p + p * err
 
     return round(p), round(p_lo), round(p_hi)
 
 
-def eta_simple(dj, v=80, err=0.1):
+def eta_simple(d, v=80, err=0.1):
     """
-    Simplistic transportation time estimate
-    time, t [h] = distance, d [km] / speed, v [km/h]
+    Simplistic transportation time estimate.
+    time t [h], lane length l [km], and speed v [km/h]
     """
-    d = dist(dj)
+    l = dist(d)
 
-    t = d / v
+    t = l / v
     t_lo = t - t * err
     t_hi = t + t * err
 
     return round(t, 1), round(t_lo, 1), round(t_hi, 1)
 
 
-def co_simple(dj, c=100, err=0.2):
+def co_simple(d, c=100, err=0.2):
     """
-    Simplistic transportation CO2 exhaust estimate
-    CO2 [g] =  distance, d [km] * CO2, c [g/km]
+    Simplistic transportation CO2 [g] estimate
+    distance d [km], CO2/km c [g/km]
     """
-    d = dist(dj)
+    l = dist(d)
 
-    co = d * c
+    co = c * l
     co_lo = co - co * err
     co_hi = co + co * err
 
