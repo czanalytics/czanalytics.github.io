0cd09f7aeb8a4ea0793a29f16cb55ae590a525b6
czanalytics <aatos.heikkinen@czanalytics.com>
2023-07-20 12:36:42 +0300

    testing 3 new towns

--- lt/api.sh
+++ lt/api.sh
@@ -41,6 +41,10 @@ api_model() {
 
  id="230801-001"
  da="23-08-01"
+ 
+ laam=52.36760; loam=4.90410  # amsterdam
+ lalo=51.50720; lolo=0.12760  # london
+ laco=55.67610; loco=12.56830 # copenhagen
  lapa=48.86471; lopa=2.23901  # paris
  lawa=52.23704; lowa=21.01753 # warsaw
  labe=52.52000; lobe=13.40495 # berlin
@@ -58,22 +62,27 @@ api_model() {
  mehest="helsinki-stockholm"
  meheta="helsinki-tallinn"
  mehebe="helsinki-berlin"
+ mecoam="copenhagen-amsterdam"
+ mepalo="paris-london"
+ mepaam="paris-amsterdam"
 
  dtemp='{"id":"%s", "da":"%s", "lat1":%s, "lon1":%s, "lat2":%s, "lon2":%s, "meta":"%s"}\n' # careful
 
- d1=$(printf "$dtemp" "$id" "$da" "$lapa" "$lopa" "$lawa" "$lowa" "$mepawa")
- d2=$(printf "$dtemp" "$id" "$da" "$lapa" "$lopa" "$labe" "$lobe" "$mepabe")
- d3=$(printf "$dtemp" "$id" "$da" "$labe" "$lobe" "$lawa" "$lowa" "$mebewa")
- d4=$(printf "$dtemp" "$id" "$da" "$lahe" "$lohe" "$lala" "$lola" "$mehela")
- d5=$(printf "$dtemp" "$id" "$da" "$lahe" "$lohe" "$laou" "$loou" "$meheou")
- d6=$(printf "$dtemp" "$id" "$da" "$lahe" "$lohe" "$last" "$lost" "$mehest")
- d7=$(printf "$dtemp" "$id" "$da" "$lahe" "$lohe" "$lata" "$lota" "$meheta")
- d8=$(printf "$dtemp" "$id" "$da" "$lahe" "$lohe" "$labe" "$lobe" "$mehebe")
+ d1=$(printf  "$dtemp" "$id" "$da" "$lapa" "$lopa" "$lawa" "$lowa" "$mepawa")
+ d2=$(printf  "$dtemp" "$id" "$da" "$lapa" "$lopa" "$labe" "$lobe" "$mepabe")
+ d3=$(printf  "$dtemp" "$id" "$da" "$labe" "$lobe" "$lawa" "$lowa" "$mebewa")
+ d4=$(printf  "$dtemp" "$id" "$da" "$lahe" "$lohe" "$lala" "$lola" "$mehela")
+ d5=$(printf  "$dtemp" "$id" "$da" "$lahe" "$lohe" "$laou" "$loou" "$meheou")
+ d6=$(printf  "$dtemp" "$id" "$da" "$lahe" "$lohe" "$last" "$lost" "$mehest")
+ d7=$(printf  "$dtemp" "$id" "$da" "$lahe" "$lohe" "$lata" "$lota" "$meheta")
+ d8=$(printf  "$dtemp" "$id" "$da" "$lahe" "$lohe" "$labe" "$lobe" "$mehebe")
+ d9=$(printf  "$dtemp" "$id" "$da" "$laco" "$loco" "$laam" "$loam" "$mecoam")
+ d10=$(printf "$dtemp" "$id" "$da" "$lapa" "$lopa" "$laam" "$loam" "$mepaam")
+ #d11=$(printf "$dtemp" "$id" "$da" "$lapa" "$lopa" "$lalo" "$lolo" "$mepalo") # ERROR NUTS reqion not found??
  
 #d1='{"id":"230701-001",                 "da":"23-07-01",                                          "lat1":60.19205,"lon1":24.94583,"lat2":60.10549,"lon2":24.15589}'
 #d2='{"id":"230701-001",        "co":100,"da":"23-07-01",                                          "lat1":60.19205,"lon1":24.94583,"lat2":60.10549,"lon2":24.15589}'
 #d3='{"id":"230701-001","seg":1,"co":100,"da":"23-07-01","ta":"10:00","db":"23-07-01","tb":"12:00","lat1":60.19205,"lon1":24.94583,"lat2":60.10549,"lon2":24.15589}'
-#d4='{"id":"230702-001","seg":1,"co":100,"da":"23-07-02","ta":"10:00","db":"23-07-02","tb":"12:00","lat1":60.19205,"lon1":24.94583,"lat2":60.30549,"lon2":24.35589}'
 
 api_local() { 
  echo "test api functionality"
@@ -107,7 +116,7 @@ api_local() {
  curl -H "$key" -s "$url"     | "$pp" # request pp with silent -s
  curl -H "$key" -s "$url"/api | "$pp" 
  
- for i in {1..8}
+ for i in {1..10}
  do
    di="d$i"         # test selected
    d=$(echo ${!di}) # evaluated
@@ -194,7 +203,6 @@ api_config() {
  dc1='{"id":"230701-001",                 "da":"23-07-01",                                          "lat1":60.19205,"lon1":24.94583,"lat2":60.10549,"lon2":24.15589}'
  dc2='{"id":"230701-001",        "co":100,"da":"23-07-01",                                          "lat1":60.19205,"lon1":24.94583,"lat2":60.10549,"lon2":24.15589}'
  dc3='{"id":"230701-001","seg":1,"co":100,"da":"23-07-01","ta":"10:00","db":"23-07-01","tb":"12:00","lat1":60.19205,"lon1":24.94583,"lat2":60.10549,"lon2":24.15589}'
- dc4='{"id":"230702-001","seg":1,"co":100,"da":"23-07-02","ta":"10:00","db":"23-07-02","tb":"12:00","lat1":60.19205,"lon1":24.94583,"lat2":60.30549,"lon2":24.35589}'
 
  curl -H "$key" -s "$url"/api/config | "$pp" 
 
@@ -230,7 +238,6 @@ api_status() {
  ds1='{"id":"230701-001",                 "da":"23-07-01",                                          "lat1":60.19205,"lon1":24.94583,"lat2":60.10549,"lon2":24.15589}'
  ds2='{"id":"230701-001",        "co":100,"da":"23-07-01",                                          "lat1":60.19205,"lon1":24.94583,"lat2":60.10549,"lon2":24.15589}'
  ds3='{"id":"230701-001","seg":1,"co":100,"da":"23-07-01","ta":"10:00","db":"23-07-01","tb":"12:00","lat1":60.19205,"lon1":24.94583,"lat2":60.10549,"lon2":24.15589}'
- ds4='{"id":"230702-001","seg":1,"co":100,"da":"23-07-02","ta":"10:00","db":"23-07-02","tb":"12:00","lat1":60.19205,"lon1":24.94583,"lat2":60.30549,"lon2":24.35589}'
 
  curl -H "$key" -s "$url"/api/status | "$pp" 
 
@@ -277,7 +284,6 @@ api_report() {
  
  dr2='{"id":"230701-001",        "co":100,"da":"23-07-01",                                          "lat1":60.19205,"lon1":24.94583,"lat2":60.10549,"lon2":24.15589}'
  dr3='{"id":"230701-001","seg":1,"co":100,"da":"23-07-01","ta":"10:00","db":"23-07-01","tb":"12:00","lat1":60.19205,"lon1":24.94583,"lat2":60.10549,"lon2":24.15589}'
- dr4='{"id":"230702-001","seg":1,"co":100,"da":"23-07-02","ta":"10:00","db":"23-07-02","tb":"12:00","lat1":60.19205,"lon1":24.94583,"lat2":60.30549,"lon2":24.35589}'
 
  curl -H "$key" -s "$url"/api/report | "$pp" 
 
