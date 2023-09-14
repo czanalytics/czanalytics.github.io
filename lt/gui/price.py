32693704e1ec720b58c5e1487265abf44c0aef22
czanalytics <aatos.heikkinen@czanalytics.com>
2023-09-14 11:32:26 +0200

    functions moved to price.py

--- lt/gui/price.py
+++ lt/gui/price.py
@@ -1,28 +1,64 @@
+# price.py
+
+def approx(x, n=20):
+  """Round x to closest n"""
+
+  return round(x/n)*n
+
+def price_est(loc1, loc2, dist = 111, c = 2.0, pro=0.1):
+  """Estimate price based on postal code locations."""
+
+  p = dist * c             # mockup
+  p_lo = p - pro * p
+  p_hi = p + pro * p
+
+  return approx(p), approx(p_lo), approx(p_hi)
+
+
+import pandas as pd
+
+from pyodide.http import open_url
+
+#url_nuts2 = 'https://raw.githubusercontent.com/czanalytics/multimodal-shipping/main/dat/nuts2.json'
+#geo_nuts2= json.loads(open_url(url_nuts2).read())
+
+def get_data():
+    url = ("https://raw.githubusercontent.com/czanalytics/multimodal-shipping/main/dat/ODMatrix2021_N2.csv")
+    d = pd.read_csv(open_url(url))
+
+    return d
+
+#d_ = d.loc[(d['Origin'] == 'ES70') & (d['Destination'] == 'PT11') ]
+#print (d_)
+
+
+import js
 from js import document
-    #This is necessary for reasons I don't understand
-    from pyodide import create_proxy
+from pyodide.ffi import create_proxy
 
-    def _flight_mode_change(*args, **kwargs):
-        currentMode = document.getElementById("flight-mode-select").value
-        if currentMode == 'one':
-            document.getElementById("ret").disabled = True
-        else:
-            document.getElementById("ret").disabled = False
+def _flight_mode_change(*args, **kwargs):
+  currentMode = document.getElementById("flight-mode-select").value
 
-    flight_mode_change = create_proxy(_flight_mode_change)
-    document.getElementById("flight-mode-select").addEventListener("input", flight_mode_change)
+  currentMode = 'eur'
 
-    def _book_flight(*args, **kwargs):
-        currentMode = document.getElementById("flight-mode-select").value
-        departure = document.getElementById("dep").value
-        return_flight = document.getElementById("ret").value
+  if currentMode == 'one':
+    document.getElementById("ret").disabled = True
+  else:
+    document.getElementById("ret").disabled = False
 
-        if currentMode == 'one':
-            document.getElementById("flight-info").innerText = f"You've booked a one-way flight departing on {departure}."
-        else:
-            document.getElementById("flight-info").innerText = f"You've booked a round-trip flight departing on {departure} and returning on {return_flight}."
 
-    book_flight = create_proxy(_book_flight)
-    document.getElementById("book-flight").addEventListener("click", book_flight)
+def _book_flight(*args, **kwargs):
+  currentMode = document.getElementById("flight-mode-select").value
+  currentMode = 'eur'
 
-    flight_mode_change()
+  loc1 = document.getElementById("dep").value
+  loc2 = document.getElementById("ret").value
+
+  price, price_lo, price_hi = price_est(loc1, loc2)
+  #price = str(1000); price_lo = 'aaa'; price_hi = 'sssss'
+
+  if currentMode == 'one':
+    document.getElementById("flight-info").innerText = f"Departing on {loc1}."
+  else:
+    document.getElementById("flight-info").innerText = f"{price} EUR ({price_lo}, {price_hi})"
+
